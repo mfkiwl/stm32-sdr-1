6dbbcd9e5e3cb632f5f5e0c29dc8792979a8ca6d
knightshrub <yuma@knightshrub.xyz>
2020-08-16 15:21:23 +0200

    set frequency in kHz

--- setfreq.py
+++ setfreq.py
@@ -4,5 +4,5 @@ import serial
 import sys
 
 with serial.Serial("/dev/ttyACM0", 115200) as ser:
-    freq = int(float(sys.argv[1])*1e6)
+    freq = int(float(sys.argv[1])*1e3)
     ser.write(f"#{freq}\n".encode("utf-8"))
